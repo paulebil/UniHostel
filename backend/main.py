e34d2d8dd632a665b076f31ad7609568fbc59a59
paulebil <ebilpaul06@gmail.com>
2025-03-17 08:50:16 +0300

    added routers to the main function

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from backend.app.routes.users import user_router
+from backend.app.routes.users import user_router, auth_router, guest_router
 
 
 
 def create_application():
     application = FastAPI()
     application.include_router(user_router)
+    application.include_router(guest_router)
+    application.include_router(auth_router)
     return application
 
 app = create_application()
