b874c632c63845b74e4bd7695eea4a06af249f7e
paulebil <ebilpaul06@gmail.com>
2025-03-25 17:02:38 +0300

    created hostel repository

--- backend/app/repository/hostels.py
+++ backend/app/repository/hostels.py
@@ -5,9 +5,34 @@ from sqlalchemy.exc import IntegrityError
 from backend.app.models.hostels import Hostel
 
 
+
 class HostelRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def create_hostel(self, data):
-        pass
+    def create_hostel(self, hostel: Hostel):
+        try:
+            self.session.add(hostel)
+            self.session.commit()
+            self.session.refresh(hostel)
+        except IntegrityError:
+            self.session.rollback()
+            raise
+
+
+    def update_hostel(self, hostel: Hostel):
+        try:
+            self.session.merge(hostel)
+            self.session.commit()
+            self.session.refresh(hostel)
+        except IntegrityError:
+            self.session.rollback()
+            raise
+
+    def delete_hostel(self, hostel: Hostel):
+        self.session.delete(hostel)
+        self.session.commit()
+
+    def get_all_hostels(self):
+        return self.session.query(Hostel).all()
+
