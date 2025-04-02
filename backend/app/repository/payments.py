d9604d261222a1d653509c4c98d3c2ba4d0b68f4
paulebil <ebilpaul06@gmail.com>
2025-04-02 14:14:04 +0300

    created payment repository

---
+++ backend/app/repository/payments.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from backend.app.models.payments import Payment
+
+
+class PaymentRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def make_payment(self, payment: Payment):
+        try:
+            self.session.add(payment)
+            self.session.commit()
+            self.session.refresh(payment)
+        except IntegrityError:
+            self.session.rollback()
+            raise
+
+    def update_payment(self, payment: Payment):
+        try:
+            self.session.merge(payment)
+            self.session.commit()
+            self.session.refresh(payment)
+        except IntegrityError:
+            self.session.rollback()
+            raise
+
+    def get_payment_by_id(self, payment_id: int):
+        return self.session.query(Payment).filter(Payment.id == payment_id).first()
+
+    def get_payment_by_transaction_id(self, transaction_id: str):
+        return self.session.query(Payment).filter(Payment.transaction_id == transaction_id).first()
